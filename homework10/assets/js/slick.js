c9ab54d1e4329bf3b93f5202a1045536247e0a39
Denis, Makarov <makarovdenis07081985@gmail.com>
2018-03-29 13:27:07 +0300

    all lesson 10

---
+++ homework10/assets/js/slick.js
@@ -0,0 +1,10 @@
+
+$('.slider').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+
+  prevArrow: '<button class="arrow_left" type="button"><i class="fas fa-chevron-left"></i></button>',
+  nextArrow: '<button class="arrow_right" type="button"><i class="fas fa-chevron-right"></i></button>'
+});
